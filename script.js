0eb54e1fa774a5801f2f94f85327ccc51597d165
Kari Meghana <karimeghana02@gmail.com>
2025-07-04 11:16:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
 //your code here
-document.addEventListener("DOMContentLoaded", function() {
-    const songName = "Never Gonna Give You Up by Rick Astley";
-    const songLink = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-    document.getElementById("song-link").textContent = songName;
-    document.getElementById("song-link").href = songLink;
-});
